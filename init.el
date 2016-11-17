364087531e5ec1871d4f9a4789fff03acebf1f25
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-11-17 10:59:00 +0200

    Simplified init

--- init.el
+++ init.el
@@ -1,29 +1,35 @@
 ;; init.el: Emacs configuration of Dimiter Naydenov.
 ;;
 
-;; User Info
-(setq user-full-name "Dimiter Naydenov")
-(setq user-mail-address "dimiter@naydenov.net")
-
-;; Please don't load outdated byte code
-(setq load-prefer-newer t)
-
-;; Install use-package if necessary
+;; Initialize package, which also sets load-path, needed before setting the
+;; package-archives below.
 (require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives (append package-archives
-			 '(("melpa" . "http://melpa.org/packages/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")
-			 ("elpy" . "http://jorgenschaefer.github.io/packages/"))))
 (package-initialize)
 
-;; Bootstrap `use-package'
+(setq
+ ;; User Info
+ user-full-name "Dimiter Naydenov"
+ user-mail-address "dimiter@naydenov.net"
+ ;; Packaging
+ load-prefer-newer t              ; don't load outdated byte code.
+ package-enable-at-startup nil    ; don't activate installed packages.
+ package-archives (append package-archives
+			  '(("melpa" . "http://melpa.org/packages/")
+			    ("gnu" . "http://elpa.gnu.org/packages/")
+			    ("elpy" . "http://jorgenschaefer.github.io/packages/")))
+ ;; use-package - log what gets loaded.
+ use-package-verbose t
+ ;; Increase the numer of lines in the *Messages* buffer to help debugging init
+ ;; issues.
+ message-log-max 10000
+ )
+
+;; Install use-package if necessary
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 ;; Enable use-package
-(setq use-package-verbose t)
 (eval-when-compile
   (require 'use-package))
 (require 'diminish)                ;; if you use :diminish
@@ -31,8 +37,7 @@
 
 ;; Debugging & load-time diagnostics.
 (add-hook 'after-init-hook (lambda () (message "Time to load init file: %s"
-                                               (emacs-init-time))))
-(setq message-log-max 10000)
+					       (emacs-init-time))))
 
 ;; Personal packages config.
 (use-package dimitern-startup

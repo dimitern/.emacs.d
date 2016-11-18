8ecd67b2df5c663655cb1b2ee5ba9b41a77a72ce
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-11-18 20:47:23 +0200

    Properly configure and document backups

--- lisp/dimitern-backups.el
+++ lisp/dimitern-backups.el
@@ -2,13 +2,23 @@
 ;;
 
 (setq
- ;; Put all backups in one place.
+ ;; Put all backups, autosaves in one place.
  backup-directory-alist '(("." . "~/.emacs.d/backups"))
- ;; Save and version all backups.
- delete-old-versions -1
- version-control t
- vc-make-backup-files t
  auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t))
+ ;; Make backups, including for VC-managed files.
+ make-backup-files t
+ vc-make-backup-files t
+ ;; Version all create backups (with incremental index as suffix).
+ version-control t
+ ;; Delete old backup versions automatically.
+ delete-old-versions t
+ ;; Keep the oldest and newest 5 backups only.
+ kept-old-versions 5
+ kept-new-versions 5
+ ;; Always copy, never rename files when backing up.
+ backup-by-copying t
+ backup-by-copying-when-linked t
+ backup-by-copying-when-mismatch t
  )
 
 (provide 'dimitern-backups)

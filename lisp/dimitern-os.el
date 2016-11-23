c077a4a80a68f25b9629e0cdd1b9143e6861893f
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-11-23 12:44:08 +0200

    Added OS helpers

---
+++ lisp/dimitern-os.el
@@ -0,0 +1,11 @@
+;; dimitern-os.el: OS-specific helper functions.
+
+(defun dimitern-os/is-linux ()
+  "Returns non-nil if running on Linux OS."
+  (eq system-type 'gnu/linux))
+
+(defun dimitern-os/is-darwin ()
+  "Returns non-nil if running on Mac OS X (darwin)."
+  (eq system-type 'darwin))
+
+(provide 'dimitern-os)

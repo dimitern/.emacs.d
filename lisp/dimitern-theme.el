b0a89788703c12f42c5ff1e7cc65c346b72e0b0f
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-11-23 12:45:45 +0200

    Renamed defs to use package prefix in theme; minor simplifications

--- lisp/dimitern-theme.el
+++ lisp/dimitern-theme.el
@@ -2,33 +2,37 @@
 ;;
 
 ;; Default font (FIXME: only works on Ubuntu).
-(defvar dimitern/default-font-family "Input Mono")
-(defvar dimitern/default-font-name "Light")
-(defvar dimitern/default-font-size 9)
-(defvar dimitern/default-font-dir "fonts/Input-Font/")
+(defvar dimitern-theme/font-family "Input Mono"
+  "Font family for the theme.")
+(defvar dimitern-theme/font-name "Light"
+  "Font name for the theme.")
+(defvar dimitern-theme/font-size 9
+  "Font size for the theme.")
+(defvar dimitern-theme/font-dir "fonts/Input-Font/"
+  "Fonts directory prefix.")
 
-(defun dimitern/default-font ()
+(defun dimitern-theme/font ()
   "Default font family, name, and size to use."
   (format "%s %s-%s"
-	  dimitern/default-font-family
-	  dimitern/default-font-name
-	  dimitern/default-font-size))
+	  dimitern-theme/font-family
+	  dimitern-theme/font-name
+	  dimitern-theme/font-size))
 
-(defun dimitern/install-default-font ()
-    (let ((home-font-dir (format "~/.%s" dimitern/default-font-dir))
-	  (emacs-font-dir (format "~/.emacs.d/%s" dimitern/default-font-dir)))
-      (when (not (member dimitern/default-font-family (font-family-list)))
-	(progn
-	  (message (format "dimitern: Installing fonts in '%s'" dimitern/default-font-dir))
-	  (shell-command (format "cp -R %s %s" emacs-font-dir home-font-dir))
-	  (message "dimitern: Rebuilding fonts cache...")
-	  (shell-command (format "fc-cache -r %s" home-font-dir))))))
+(defun dimitern-theme/install-font ()
+  "Install default font when missing."
+    (let ((home-font-dir (format "~/.%s" dimitern-theme/font-dir))
+	  (emacs-font-dir (format "~/.emacs.d/%s" dimitern-theme/font-dir)))
+      (when (not (member dimitern-theme/font-family (font-family-list)))
+	(message (format "dimitern: Installing fonts in '%s'" dimitern-theme/font-dir))
+	(shell-command (format "cp -R %s %s" emacs-font-dir home-font-dir))
+	(message "dimitern: Rebuilding fonts cache...")
+	(shell-command (format "fc-cache -r %s" home-font-dir)))))
 
 ;; Install and enable solarized dark theme, installling font if needed.
 (use-package solarized-theme
   :if (or
        (display-graphic-p)
-       (eq system-type 'darwin))
+       (dimitern-os/is-darwin))
   :ensure t
   :init
   (setq
@@ -36,12 +40,12 @@
    font-use-system-font t ;; needed for fonts installation to work.
    solarized-termcolors 256
    )
-  (unless (member dimitern/default-font-family (font-family-list))
+  (unless (member dimitern-theme/font-family (font-family-list))
     ;; Font installation only works on Linux.
-    (when (not (eq system-type 'darwin))
-      (dimitern/install-default-font)
-      (add-to-list 'initial-frame-alist (cons 'font (dimitern/default-font)))
-      (add-to-list 'default-frame-alist (cons 'font (dimitern/default-font)))))
+    (unless (dimitern-os/is-darwin)
+      (dimitern-theme/install-font)
+      (add-to-list 'initial-frame-alist (cons 'font (dimitern-theme/font)))
+      (add-to-list 'default-frame-alist (cons 'font (dimitern-theme/font)))))
   (customize-set-variable 'frame-background-mode 'dark)
   (load-theme 'solarized-dark 'no-confirm 'no-enable)
   :config

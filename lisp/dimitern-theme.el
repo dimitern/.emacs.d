6f489814c3dace341abd739b5849df8acc8e88ae
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-12-25 15:51:43 +0200

    Cleaned up whitespace.

--- lisp/dimitern-theme.el
+++ lisp/dimitern-theme.el
@@ -14,14 +14,14 @@
 (defun dimitern-theme/font ()
   "Default font family, name, and size to use."
   (format "%s %s-%s"
-	  dimitern-theme/font-family
-	  dimitern-theme/font-name
-	  dimitern-theme/font-size))
+          dimitern-theme/font-family
+          dimitern-theme/font-name
+          dimitern-theme/font-size))
 
 (defun dimitern-theme/install-font ()
   "Install default font when missing."
   (let ((home-font-dir (format "~/.%s" dimitern-theme/font-dir))
-	(emacs-font-dir (format "~/.emacs.d/%s" dimitern-theme/font-dir)))
+        (emacs-font-dir (format "~/.emacs.d/%s" dimitern-theme/font-dir)))
     (when (not (member dimitern-theme/font-family (font-family-list)))
       (message (format "dimitern: Installing fonts in '%s'" dimitern-theme/font-dir))
       (shell-command (format "cp -R %s %s" emacs-font-dir home-font-dir))
@@ -31,15 +31,15 @@
 (defvar dimitern-theme/theme nil
   "Selected default theme depending on system-type.")
 
-(defun dimitern-theme/load-default () 
+(defun dimitern-theme/load-default ()
   "Load the default theme, depending on system-type."
   (if (dimitern-os/is-darwin)
       ;; solarized-light for darwih, with bigger font size.
       (progn
-	(load-theme 'solarized-light 'no-confirm 'no-enable)
-	(customize-set-variable 'frame-background-mode 'light)
-	(setq dimitern-theme/theme 'solarized-light)
-	(set-face-attribute 'default nil :height (* 10 (frame-char-height))))
+        (load-theme 'solarized-light 'no-confirm 'no-enable)
+        (customize-set-variable 'frame-background-mode 'light)
+        (setq dimitern-theme/theme 'solarized-light)
+        (set-face-attribute 'default nil :height (* 10 (frame-char-height))))
     ;; solarized-dark for linux.
     (load-theme 'solarized-dark 'no-confirm 'no-enable)
     (validate-setq
@@ -49,18 +49,18 @@
      )
     ;; Install font if needed (linux only).
     (if (member dimitern-theme/font-family (font-family-list))
-	t
+        t
       (when (display-graphic-p)
-	;; Font installation only works on Linux.
-	(dimitern-theme/install-font)
-	(add-to-list 'initial-frame-alist (cons 'font (dimitern-theme/font)))
-	(add-to-list 'default-frame-alist (cons 'font (dimitern-theme/font)))))))
+        ;; Font installation only works on Linux.
+        (dimitern-theme/install-font)
+        (add-to-list 'initial-frame-alist (cons 'font (dimitern-theme/font)))
+        (add-to-list 'default-frame-alist (cons 'font (dimitern-theme/font)))))))
 
 ;; Solarized theme config.
 (use-package solarized-theme
   :ensure t
   :after frame
-  :config  
+  :config
   (dimitern-theme/load-default)
   (validate-setq
    ;; Disable variable pitch fonts in Solarized theme

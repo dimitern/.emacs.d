5a14ebc5efb93e9279334ab22c3a878bc08cf39f
Dimiter Naydenov <dimiter@users.noreply.github.com>
2019-11-05 08:52:04 +0200

    More fixes

--- custom.el
+++ custom.el
@@ -36,20 +36,25 @@
  '(company-tooltip-align-annotations t)
  '(company-tooltip-flip-when-above t)
  '(company-tooltip-minimum-width 40)
- '(custom-enabled-themes (quote (doom-tomorrow-day)))
+ '(custom-enabled-themes (quote (doom-vibrant)))
  '(custom-safe-themes
    (quote
     ("fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "49ec957b508c7d64708b40b0273697a84d3fee4f15dd9fc4a9588016adee3dad" "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "a8c210aa94c4eae642a34aaf1c5c0552855dfca2153fa6dd23f3031ce19453d4" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "7e559cd74b715c744ff8cf276a774e4a087ae7065062cb258bc3ef30faecb55b" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "c1ab17435aaac39206b0f34dddda6349d91004c27258ab55301b973b179ddd55" "b63c0298290e6c6aa17d64fbce2c2a83c4b420d73c378f4f96dd8e1883ac6d51" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" default)))
+ '(diff-switches "-u -a")
  '(doom-modeline-mode t)
  '(ein:default-url-or-port "http://127.0.0.1:8889")
  '(ein:enable-keepalive t)
  '(ein:url-or-port (quote ("http://127.0.0.1:8889")))
  '(enable-local-variables :safe)
+ '(exec-path
+   (quote
+    ("c:/Python27/Lib/site-packages/PyQt4" "c:/emacs/bin" "C:/Windows/system32" "C:/Windows" "C:/Windows/System32/Wbem" "C:/Windows/System32/WindowsPowerShell/v1.0/" "C:/Program Files/Git/cmd" "C:/Program Files (x86)/Aspell/bin" "C:/Program Files (x86)/Graphviz2.38/bin" "C:/Program Files (x86)/Microsoft SQL Server/Client SDK/ODBC/110/Tools/Binn/" "C:/Program Files (x86)/Microsoft SQL Server/120/Tools/Binn/" "C:/Program Files (x86)/Microsoft SQL Server/120/DTS/Binn/" "C:/Program Files (x86)/Microsoft SQL Server/120/Tools/Binn/ManagementStudio/" "c:/python27" "c:/python27/scripts" "c:/emacs/libexec/emacs/25.1/x86_64-w64-mingw32")))
+ '(explicit-shell-file-name "c:/Windows/System32/cmd.exe")
  '(fci-rule-color "#37474f")
- '(flycheck-disabled-checkers (quote (python-pylint python-pycompile)))
  '(flycheck-checkers
    (quote
     (ada-gnat asciidoctor asciidoc c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint css-stylelint cwl d-dmd dockerfile-hadolint elixir-dogma emacs-lisp emacs-lisp-checkdoc erlang-rebar3 erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert go-megacheck groovy haml handlebars haskell-stack-ghc haskell-ghc haskell-hlint html-tidy javascript-eslint javascript-jshint javascript-standard json-jsonlint json-python-json jsonnet less less-stylelint llvm-llc lua-luacheck lua perl perl-perlcritic php php-phpmd php-phpcs processing proselint protobuf-protoc pug puppet-parser puppet-lint r-lintr racket rpm-rpmlint markdown-mdl nix rst-sphinx rst ruby-rubocop ruby-reek ruby-rubylint ruby ruby-jruby rust-cargo rust scala scala-scalastyle scheme-chicken scss-lint scss-stylelint sass/scss-sass-lint sass scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint systemd-analyze tcl-nagelfar tex-chktex tex-lacheck texinfo typescript-tslint verilog-verilator xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby python-mypy python-flake8)))
+ '(flycheck-disabled-checkers (quote (python-pylint python-pycompile)))
  '(flycheck-display-errors-function nil)
  '(flycheck-executable-find (quote flycheck-default-executable-find))
  '(flycheck-flake8-maximum-complexity 10)
@@ -69,7 +74,8 @@
     ("--follow-imports=silent" "--ignore-missing-imports")))
  '(flycheck-python-mypy-executable "mypy")
  '(flycheck-syntax-check-failed-hook (quote (ignore)))
- '(frame-background-mode (quote dark))
+ '(frame-background-mode (quote light))
+ '(fringe-mode (quote (16 . 0)) nil (fringe))
  '(global-nlinum-mode t)
  '(go-command "$GOROOT/bin/go")
  '(hippie-expand-ignore-buffers (quote ("^ \\*.*\\*$" dired-mode)))
@@ -87,7 +93,7 @@
  '(json-reformat:indent-width 2)
  '(linum-delay t)
  '(linum-eager nil)
- '(magit-git-debug t t)
+ '(magit-git-debug t)
  '(markdown-asymmetric-header t)
  '(markdown-css-paths
    (quote
@@ -95,24 +101,6 @@
  '(markdown-enable-html nil)
  '(mouse-wheel-scroll-amount (quote (0.07)))
  '(nlinum-highlight-current-line t)
- '(nlinum-use-right-margin t)
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/org-home/newgtd.org" "~/Dropbox/org-home/birthday.org")))
- '(package-selected-packages
-   (quote
-    (haml-mode pycoverage ov py-isort nginx-mode nginx company-racer company-rust flycheck-rust racer rust-mode rust flycheck-mypy material-light material ein go-errcheck errcheck go-mode pydoc multiple-cursors package-lint py-autopep8 org-mobile org-mode holiday org-capture org-bullets material-theme gitattributes-mode gitignore-mode gitconfig-mode magit rainbow-delimiters ibuffer-projectile virtualenvwrapper ace-link avy ivy-pages visual-fill-column adaptive-wrap whitespace-cleanup-mode easy-kill counsel-projectile projectile js2-mode web-mode pip-requirements company-anaconda anaconda-mode graphviz-dot-mode json-mode markdown-mode flycheck-title flycheck-pos-tip flycheck auto-dictionary company-emoji company-math company-statistics company-quickhelp company yasnippet rainbow-mode highlight-numbers highlight-symbol hl-todo expand-region undo-tree smartparens nlinum reveal-in-osx-finder sudo-edit launch hardhat ignoramus neotree golden-ratio ace-window ibuffer-vc focus-autosave-mode stripe-buffer beacon page-break-lines yaml-mode swiper wgrep-ag ag hydra visual-regexp anzu paradox counsel ivy-hydra ivy spaceline which-key exec-path-from-shell validate solarized-theme use-package)))
- '(diff-switches "-u -a")
- '(exec-path
-   (quote
-    ("c:/Python27/Lib/site-packages/PyQt4" "c:/emacs/bin" "C:/Windows/system32" "C:/Windows" "C:/Windows/System32/Wbem" "C:/Windows/System32/WindowsPowerShell/v1.0/" "C:/Program Files/Git/cmd" "C:/Program Files (x86)/Aspell/bin" "C:/Program Files (x86)/Graphviz2.38/bin" "C:/Program Files (x86)/Microsoft SQL Server/Client SDK/ODBC/110/Tools/Binn/" "C:/Program Files (x86)/Microsoft SQL Server/120/Tools/Binn/" "C:/Program Files (x86)/Microsoft SQL Server/120/DTS/Binn/" "C:/Program Files (x86)/Microsoft SQL Server/120/Tools/Binn/ManagementStudio/" "c:/python27" "c:/python27/scripts" "c:/emacs/libexec/emacs/25.1/x86_64-w64-mingw32")))
- '(explicit-shell-file-name "c:/Windows/System32/cmd.exe")
- '(frame-background-mode (quote light))
- '(fringe-mode (quote (16 . 0)) nil (fringe))
- '(magit-git-debug t)
- '(package-selected-packages
-   (quote
-    (pdbtrack org-mode holiday org-capture org-bullets material-theme gitattributes-mode gitignore-mode gitconfig-mode magit rainbow-delimiters ibuffer-projectile virtualenvwrapper ace-link avy ivy-pages visual-fill-column adaptive-wrap whitespace-cleanup-mode easy-kill counsel-projectile projectile js2-mode web-mode pip-requirements company-anaconda anaconda-mode graphviz-dot-mode json-mode markdown-mode flycheck-title flycheck-pos-tip flycheck auto-dictionary company-emoji company-math company-statistics company-quickhelp company yasnippet rainbow-mode highlight-numbers highlight-symbol hl-todo expand-region undo-tree smartparens nlinum reveal-in-osx-finder sudo-edit launch hardhat ignoramus neotree golden-ratio ace-window ibuffer-vc focus-autosave-mode stripe-buffer beacon page-break-lines yaml-mode swiper wgrep-ag ag hydra visual-regexp anzu paradox counsel ivy-hydra ivy spaceline which-key exec-path-from-shell validate solarized-theme use-package)))
  '(nlinum-use-right-margin nil)
  '(notmuch-search-oldest-first nil)
  '(notmuch-show-relative-dates nil)
@@ -158,8 +146,7 @@
      (py-isort-mode . t)
      (blacken-mode . t)
      (pythonic-activate . \.venv)
-     (flycheck-checker . python-flake8)
-)))
+     (flycheck-checker . python-flake8))))
  '(sentence-end-double-space nil)
  '(show-smartparens-global-mode t)
  '(standard-indent 2)
